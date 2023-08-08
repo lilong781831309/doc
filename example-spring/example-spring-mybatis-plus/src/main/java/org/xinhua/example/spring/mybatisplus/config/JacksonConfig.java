c07a2c64ea0ec86fbc8ff0a1307ac6762d1b094c
李龙 <15200584536@163.com>
2023-08-09 04:34:22 +0800

    Jackson2ObjectMapperBuilderCustomizer、RestTemplateCustomizer

--- example-spring/example-spring-mybatis-plus/src/main/java/org/xinhua/example/spring/mybatisplus/config/JacksonConfig.java
+++ example-spring/example-spring-mybatis-plus/src/main/java/org/xinhua/example/spring/mybatisplus/config/JacksonConfig.java
@@ -1,9 +1,8 @@
 package org.xinhua.example.spring.mybatisplus.config;
 
-import cn.hutool.core.date.DatePattern;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
@@ -12,56 +11,44 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-import java.util.TimeZone;
 
 @Configuration
-@AutoConfigureBefore(JacksonAutoConfiguration.class)
 public class JacksonConfig {
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setLocale(Locale.CHINA);
-        objectMapper.setTimeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
-        objectMapper.registerModule(javaTimeModule());
-        objectMapper.setDateFormat(new SimpleDateFormat(DatePattern.NORM_DATETIME_PATTERN));
-        // 忽略json字符串中存在但bean中不存在的属性
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        // 空对象不抛异常
-        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        // 不使用默认时间格式，使用自定义时间格式
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        // 序列化所有对象属性
-        objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
-        // private 到 public 修饰的都可识别
-        //objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.findAndRegisterModules();
+    public static final String DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    public static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern(DATETIME_PATTERN);
 
-        return objectMapper;
+    @Order
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.modulesToInstall(javaTimeModule())
+                .simpleDateFormat(DATETIME_PATTERN)
+                .featuresToEnable(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS)
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                .featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .featuresToDisable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
+                .serializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
-    @Bean
-    public JavaTimeModule javaTimeModule() {
+    private JavaTimeModule javaTimeModule() {
         JavaTimeModule module = new JavaTimeModule();
-        DateTimeFormatter formatter = DatePattern.NORM_DATETIME_FORMATTER;
-        module.addSerializer(new LocalDateSerializer(formatter));
-        module.addSerializer(new LocalTimeSerializer(formatter));
-        module.addSerializer(new LocalDateTimeSerializer(formatter));
-        module.addDeserializer(LocalDate.class, new LocalDateDeserializer(formatter));
-        module.addDeserializer(LocalTime.class, new LocalTimeDeserializer(formatter));
-        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(formatter));
+        module.addSerializer(new LocalDateSerializer(DATETIME_FORMATTER))
+                .addSerializer(new LocalTimeSerializer(DATETIME_FORMATTER))
+                .addSerializer(new LocalDateTimeSerializer(DATETIME_FORMATTER))
+                .addDeserializer(LocalDate.class, new LocalDateDeserializer(DATETIME_FORMATTER))
+                .addDeserializer(LocalTime.class, new LocalTimeDeserializer(DATETIME_FORMATTER))
+                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DATETIME_FORMATTER));
         return module;
     }
+
 }
--- example-spring/example-spring-mybatis-plus/src/main/java/org/xinhua/example/spring/mybatisplus/config/RestTemplateConfig.java
+++ example-spring/example-spring-mybatis-plus/src/main/java/org/xinhua/example/spring/mybatisplus/config/RestTemplateConfig.java
@@ -2,9 +2,10 @@ package org.xinhua.example.spring.mybatisplus.config;
 
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -15,34 +16,36 @@ import org.springframework.web.client.RestTemplate;
 import org.xinhua.example.spring.mybatisplus.handler.WechatResponseErrorHandler;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
 
-	@Bean
-	public RestTemplate restTemplate() {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpClientBuilder clientBuilder = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy());
-		ClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(clientBuilder.build());
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
-		MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-		jackson2HttpMessageConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+    @Bean
+    public RestTemplate wechatRestTemplate(RestTemplateBuilder builder) {
+        RestTemplate restTemplate = builder.build();
+        restTemplate.setErrorHandler(new WechatResponseErrorHandler());
+        return restTemplate;
+    }
 
-		List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
-		messageConverters.removeIf(converter -> converter instanceof StringHttpMessageConverter);
-		messageConverters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
-		messageConverters.add(jackson2HttpMessageConverter);
+    @Bean
+    public RestTemplateCustomizer restTemplateCustomizer(MappingJackson2HttpMessageConverter jackson2HttpMessageConverter) {
+        return restTemplate -> {
+            List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
+            messageConverters.removeIf(converter -> converter instanceof StringHttpMessageConverter);
+            messageConverters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
+            messageConverters.removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
+            messageConverters.add(jackson2HttpMessageConverter);
 
-		restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(factory));
-
-		restTemplate.setMessageConverters(messageConverters);
-
-		//设置ErrorHandler
-		restTemplate.setErrorHandler(new WechatResponseErrorHandler());
-
-		return restTemplate;
-	}
+            HttpClientBuilder clientBuilder = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy());
+            ClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(clientBuilder.build());
+            restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(factory));
+        };
+    }
 
 }
